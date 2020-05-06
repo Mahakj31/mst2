edfe82b845f884cafddf0c4f7d98ffabe7d6534b
Mahakj31 <64589353+Mahakj31@users.noreply.github.com>
2020-05-06 11:45:07 +0530

    Add files via upload

---
+++ corona.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM corona.mst2
+LIMIT 0, 1000
+
+-- Date: 2020-05-06 10:11
+*/
+INSERT INTO `` (`no.`,`name`,`Adhar`,`mobile`,`cadd`,`hadd`,`noofmembers`,`sick`,`meals`) VALUES (1,' kamal jain',987546225,78956324,'bhopal','delhi',4,'0',2);
+INSERT INTO `` (`no.`,`name`,`Adhar`,`mobile`,`cadd`,`hadd`,`noofmembers`,`sick`,`meals`) VALUES (2,'Priya',195008765,67089852,'delhi','mumbai',NULL,NULL,3);
